f577f42c9fbe99c861a7d1a1cbac4b5870ba9f4b
Klamkin, Michael <mklamkin3@gatech.edu>
2026-02-28 11:10:40 -0700

    runic

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,22 +2,22 @@ using Test
 
 @testset verbose = true "CoolPDLP" begin
     if get(ENV, "COOLPDLP_TEST_MOISUITE", "0") != "1"
-    @testset "Formalities" begin
-        include("formalities.jl")
-    end
-    @testset "Tutorial" begin
-        include("tutorial.jl")
-    end
-    for folder in readdir(@__DIR__)
-        isdir(joinpath(@__DIR__, folder)) || continue
-        @testset verbose = true "$folder" begin
-            for file in readdir(joinpath(@__DIR__, folder))
-                @testset "$file" begin
-                    include(joinpath(@__DIR__, folder, file))
+        @testset "Formalities" begin
+            include("formalities.jl")
+        end
+        @testset "Tutorial" begin
+            include("tutorial.jl")
+        end
+        for folder in readdir(@__DIR__)
+            isdir(joinpath(@__DIR__, folder)) || continue
+            @testset verbose = true "$folder" begin
+                for file in readdir(joinpath(@__DIR__, folder))
+                    @testset "$file" begin
+                        include(joinpath(@__DIR__, folder, file))
+                    end
                 end
             end
         end
-    end
     end
     @testset "MOI Wrapper" begin
         include("moi.jl")
