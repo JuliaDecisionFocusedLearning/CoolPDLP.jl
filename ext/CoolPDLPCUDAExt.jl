cac5a806b1826f148786311e595a23311fb4cde5
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2026-03-16 07:40:31 +0100

    Fix tests

--- ext/CoolPDLPCUDAExt.jl
+++ ext/CoolPDLPCUDAExt.jl
@@ -1,6 +1,20 @@
 module CoolPDLPCUDAExt
 
-using CUDA
-using CoolPDLP
+using CUDA.CUSPARSE: CuSparseMatrixCSC, CuSparseMatrixCSR, CuSparseMatrixCOO
+using CoolPDLP: CoolPDLP
+
+function CoolPDLP.sametype_transpose(A::CuSparseMatrixCOO{Tv, Ti}) where {Tv, Ti}
+    return CuSparseMatrixCOO(A.colInd, A.rowInd, A.nzVal, (A.dims[2], A.dims[1]), A.nnz)
+end
+
+function CoolPDLP.sametype_transpose(A::CuSparseMatrixCSC{Tv, Ti}) where {Tv, Ti}
+    At_csr = CuSparseMatrixCSR(transpose(A))
+    return CuSparseMatrixCSC(At_csr)
+end
+
+function CoolPDLP.sametype_transpose(A::CuSparseMatrixCSR{Tv, Ti}) where {Tv, Ti}
+    At_csc = CuSparseMatrixCSC(transpose(A))
+    return CuSparseMatrixCSR(At_csc)
+end
 
 end
