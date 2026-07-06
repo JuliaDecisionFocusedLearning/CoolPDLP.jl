49384fd98fc337676e0feb4184aeaf023bcce488
Simeon SCHAUB <simeon.schaub@node07.cluster.local>
2026-07-06 14:47:10 +0200

    add spmm, batched CSR

--- src/utils/mat_csr.jl
+++ src/utils/mat_csr.jl
@@ -1,3 +1,5 @@
+abstract type AbstractGPUSparseArrayCSR{T, Ti, N} <: SparseArrays.AbstractSparseArray{T, Ti, N} end
+
 """
     GPUSparseMatrixCSR
 
@@ -10,7 +12,7 @@ struct GPUSparseMatrixCSR{
         Ti <: Integer,
         V <: DenseVector{T},
         Vi <: DenseVector{Ti},
-    } <: AbstractSparseMatrix{T, Ti}
+    } <: AbstractGPUSparseArrayCSR{T, Ti, 2}
     m::Int
     n::Int
     rowptr::Vi
@@ -18,10 +20,34 @@ struct GPUSparseMatrixCSR{
     nzval::V
 end
 
+Adapt.@adapt_structure GPUSparseMatrixCSR
 Base.size(A::GPUSparseMatrixCSR) = (A.m, A.n)
 
-SparseArrays.nnz(A::GPUSparseMatrixCSR) = length(A.nzval)
-SparseArrays.nonzeros(A::GPUSparseMatrixCSR) = A.nzval
+"""
+    BatchedGPUSparseMatrixCSR
+
+# Fields
+
+$(TYPEDFIELDS)
+"""
+struct BatchedGPUSparseMatrixCSR{
+        T <: Number,
+        Ti <: Integer,
+        V <: DenseMatrix{T},
+        Vi <: DenseVector{Ti},
+    } <: AbstractGPUSparseArrayCSR{T, Ti, 3}
+    m::Int
+    n::Int
+    rowptr::Vi
+    colval::Vi
+    nzval::V
+end
+
+Adapt.@adapt_structure BatchedGPUSparseMatrixCSR
+Base.size(A::BatchedGPUSparseMatrixCSR) = (A.m, A.n, size(A.nzval, 2))
+
+SparseArrays.nnz(A::AbstractGPUSparseArrayCSR) = length(A.nzval)
+SparseArrays.nonzeros(A::AbstractGPUSparseArrayCSR) = A.nzval
 
 function Base.getindex(
         A::GPUSparseMatrixCSR{T, Ti}, i::Integer, j::Integer
@@ -41,18 +67,25 @@ function Base.getindex(
     end
 end
 
-function KernelAbstractions.get_backend(A::GPUSparseMatrixCSR)
-    return common_backend(A.rowptr, A.colval, A.nzval)
-end
-
-function Adapt.adapt_structure(to, A::GPUSparseMatrixCSR)
+function Base.view(
+        A::BatchedGPUSparseMatrixCSR{T, Ti}, ::Colon, ::Colon, k::Integer,
+    ) where {T, Ti}
     return GPUSparseMatrixCSR(
         A.m,
         A.n,
-        adapt(to, A.rowptr),
-        adapt(to, A.colval),
-        adapt(to, A.nzval)
+        A.rowptr,
+        A.colval,
+        view(A.nzval, :, k)
     )
+end
+function Base.getindex(
+        A::BatchedGPUSparseMatrixCSR{T, Ti}, i::Integer, j::Integer, k::Integer,
+    ) where {T, Ti}
+    return view(A, :, :, k)[i, j]
+end
+
+function KernelAbstractions.get_backend(A::AbstractGPUSparseArrayCSR)
+    return common_backend(A.rowptr, A.colval, A.nzval)
 end
 
 function GPUSparseMatrixCSR(A::SparseMatrixCSC{T, Ti}) where {T, Ti}
@@ -91,6 +124,24 @@ end
     c[i] = α * s + β * c[i]
 end
 
+@kernel function spbmv_csr!(
+        c::DenseMatrix{T},
+        A_rowptr::DenseVector{Ti},
+        A_colval::DenseVector{Ti},
+        A_nzval::DenseMatrix{T},
+        b::DenseVector{T},
+        α::Number,
+        β::Number
+    ) where {T, Ti}
+    i, batch_idx = @index(Global, NTuple)
+    s = zero(T)
+    for k in A_rowptr[i]:(A_rowptr[i + Ti(1)] - Ti(1))
+        j = A_colval[k]
+        s += A_nzval[k, batch_idx] * b[j]
+    end
+    c[i, batch_idx] = α * s + β * c[i, batch_idx]
+end
+
 function LinearAlgebra.mul!(
         c::V,
         A::GPUSparseMatrixCSR{T, Ti, V},
@@ -101,5 +152,80 @@ function LinearAlgebra.mul!(
     backend = common_backend(c, A, b)
     kernel! = spmv_csr!(backend)
     kernel!(c, A.rowptr, A.colval, A.nzval, b, α, β; ndrange = size(A, 1))
+    return c
+end
+
+function LinearAlgebra.mul!(
+        c::M,
+        A::BatchedGPUSparseMatrixCSR{T, Ti, M},
+        b::V,
+        α::Number,
+        β::Number
+    ) where {T <: Number, Ti, M <: DenseMatrix{T}, V <: DenseVector{T}}
+    backend = common_backend(c, A, b)
+    kernel! = spbmv_csr!(backend)
+    kernel!(c, A.rowptr, A.colval, A.nzval, b, α, β; ndrange = size(c))
+    return c
+end
+
+@kernel function spmm_csr!(
+        c::DenseMatrix{T},
+        A_rowptr::DenseVector{Ti},
+        A_colval::DenseVector{Ti},
+        A_nzval::DenseVector{T},
+        b::DenseMatrix{T},
+        α::Number,
+        β::Number
+    ) where {T, Ti}
+    i, batch_idx = @index(Global, NTuple)
+    s = zero(T)
+    for k in A_rowptr[i]:(A_rowptr[i + Ti(1)] - Ti(1))
+        j = A_colval[k]
+        s += A_nzval[k] * b[j, batch_idx]
+    end
+    c[i, batch_idx] = α * s + β * c[i, batch_idx]
+end
+
+@kernel function spbmm_csr!(
+        c::DenseMatrix{T},
+        A_rowptr::DenseVector{Ti},
+        A_colval::DenseVector{Ti},
+        A_nzval::DenseMatrix{T},
+        b::DenseMatrix{T},
+        α::Number,
+        β::Number
+    ) where {T, Ti}
+    i, batch_idx = @index(Global, NTuple)
+    s = zero(T)
+    for k in A_rowptr[i]:(A_rowptr[i + Ti(1)] - Ti(1))
+        j = A_colval[k]
+        s += A_nzval[k, batch_idx] * b[j, batch_idx]
+    end
+    c[i, batch_idx] = α * s + β * c[i, batch_idx]
+end
+
+function LinearAlgebra.mul!(
+        c::M,
+        A::GPUSparseMatrixCSR{T, Ti, V},
+        b::M,
+        α::Number,
+        β::Number
+    ) where {T <: Number, Ti, M <: DenseMatrix{T}, V <: DenseVector{T}}
+    backend = common_backend(c, A, b)
+    kernel! = spmm_csr!(backend)
+    kernel!(c, A.rowptr, A.colval, A.nzval, b, α, β; ndrange = size(c))
+    return c
+end
+
+function LinearAlgebra.mul!(
+        c::M,
+        A::BatchedGPUSparseMatrixCSR{T, Ti, M},
+        b::M,
+        α::Number,
+        β::Number
+    ) where {T <: Number, Ti, M <: DenseMatrix{T}}
+    backend = common_backend(c, A, b)
+    kernel! = spbmm_csr!(backend)
+    kernel!(c, A.rowptr, A.colval, A.nzval, b, α, β; ndrange = size(c))
     return c
 end
