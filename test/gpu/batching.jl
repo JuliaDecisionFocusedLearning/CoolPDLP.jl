ba4ee59f7dd59bc0de7911a4c454016bb8f6deb3
Simeon David Schaub <simeon@schaub.rocks>
2026-08-11 09:58:27 +0200

    CUDA still needs an rtol

--- test/gpu/batching.jl
+++ test/gpu/batching.jl
@@ -17,8 +17,8 @@ function test_batching(
     ) where {T <: AbstractFloat}
     rng = Xoshiro(0)
     milps, milp_batch = random_milp_batch(rng, 20, 30, 0.4, nbatch)
-    # this might be too strict, but we can always relax it
-    iterate_rtol = 0.0
+    # this might be too strict for Float32, but we can always relax it
+    iterate_rtol = 1e-6
 
     algo = PDHG(T, Int, matrix_type; backend)
     # the preconditioner only depends on `A`, so the batch and the single problems share it
